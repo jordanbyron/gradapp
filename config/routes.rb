9cfb70258aaef2b526e723a8270fe1ef682be88a
byronj3 <byronj3@3f375f3a-e948-0410-a91a-c7f4502e6309>
2008-03-31 01:41:49 +0000

    Fixed User Authentication Error
    git-svn-id: http://svn.ducksoupsoftware.com/GradApp@3 3f375f3a-e948-0410-a91a-c7f4502e6309

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
 	map.home '', :controller =>'home', :action => 'index'
+	
 	map.resources :users
-	map.resource :session
+	map.resource :session, :controller => 'session'
 	map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
 	map.signup '/signup', :controller => 'users', :action => 'new'
 	map.login '/login', :controller => 'session', :action => 'new'
