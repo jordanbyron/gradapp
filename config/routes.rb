dada17656d6ca4ddc6876da434e7b3bb9c577e2a
romanol3 <romanol3@3f375f3a-e948-0410-a91a-c7f4502e6309>
2008-04-17 22:08:49 +0000

    git-svn-id: http://svn.ducksoupsoftware.com/GradApp@32 3f375f3a-e948-0410-a91a-c7f4502e6309

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :application_requirements
 
-  map.resources :applicant_requirements
+
 
 	map.home '', :controller =>'home', :action => 'index' # Set Root To The Home Controller
 
 	map.resources :graduate_applicants
 	map.resources :departments
   map.resources :setup
-  	
-	#User Autentication Routes	
+
+	#User Autentication Routes
 	map.resources :users, :controller => 'users'
 	map.resource :session, :controller => 'session'
 	map.login '/login', :controller => 'session', :action => 'new'
