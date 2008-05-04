fb76cd4e3de292483088d5eaaced467ec117dfd8
floresj2 <floresj2@3f375f3a-e948-0410-a91a-c7f4502e6309>
2008-05-04 23:26:36 +0000

    Mail_list: textarea output section included.
    git-svn-id: http://svn.ducksoupsoftware.com/GradApp@95 3f375f3a-e948-0410-a91a-c7f4502e6309

--- app/controllers/mail_list_controller.rb
+++ app/controllers/mail_list_controller.rb
@@ -1,17 +1,21 @@
 class MailListController < ApplicationController
   before_filter :gc_required
-  
+
 	def index
-	
-			
-		
+
+
+
 	end
-	
+
 	def generate_list
-		
-		@graduate_applicants = GraduateApplicant.find(:all)
-		#params[:applicant_status_id]
-		
+
+		@graduate_applicants = GraduateApplicant.find(:all, :conditions => {:applicant_status_id => params[:graduate_applicant][:applicant_status_id]})
+
+        for graduate_applicant in @graduate_applicants
+          @text_status = ApplicantStatus.find(graduate_applicant.applicant_status_id).status
+        end
+
+
 	end
 
 end
